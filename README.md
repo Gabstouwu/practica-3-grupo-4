25420a65f6ff4959365982a5d6e0252bb78c61d2
Gabriel G.M <ggutierrez00078@ufide.ac.cr>
2024-02-29 23:19:53 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# practica-3-grupo-4
+ Repo Práctica 3 Grupo 4: Sistema Spring Boot para gestión de árboles. MySQL con tabla 'arbol', CRUD, Bootstrap. SC-403 Universidad Fidélitas
---
+++ src/test/java/com/TareaGrupal1/demo/TareaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TareaGrupal1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
