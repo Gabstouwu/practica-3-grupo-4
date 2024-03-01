e48f69392dd810adfb8842e53dbf1a7b104535c5
jvargas2345 <126547066+jvargas2345@users.noreply.github.com>
2024-02-29 23:43:56 -0600

    Cree class hola

---
+++ src/main/java/com/TareaGrupal1/demo/prueba/prueba.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.TareaGrupal1.demo.prueba;
+
+/**
+ *
+ * @author jenni
+ */
+public class prueba {
+    String "Hola";
+    
+}
